5fa1b7a426519e8d8d5142e633477f52b60fd730
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-04-22 18:29:15 +0300

    register controlled form

--- simplechat/src/App.js
+++ simplechat/src/App.js
@@ -1,13 +1,13 @@
-// import Register from "./pages/Register";
-// import Login from './pages/Login';
+import Register from "./pages/Register";
+import Login from './pages/Login';
 import Home from './pages/Home';
 import './style.scss'
 
 function App() {
     return (
-        <Home></Home>
+        // <Home></Home>
         // <Login></Login>
-        // <Register></Register>
+        <Register></Register>
     );
 }
 
--- simplechat/src/pages/Register.js
+++ simplechat/src/pages/Register.js
@@ -1,16 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Add from '../img/addImage.png'
 
 const Register = () => {
+
+  const [values, setValues] = useState({
+    displayName: '',
+    email: '',
+    password: '',
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  
+  const changeHandler = (e) => {
+      setValues(state => ({
+          ...state,
+          [e.target.name]: e.target.value //we target by name due to state with multiple values, thus, need for constant //reusable
+      }));
+  };
+
   return (
     <div className='formContainer'>
         <div className='formWrapper'>
             <span className='logo'>Chat</span>
             <span className='title'>Register</span>
-            <form>
-                <input type="text" placeholder='display name'/>
-                <input type="email" placeholder='email'/>
-                <input type="password" placeholder='******'/>
+            <form onSubmit={handleSubmit}>
+                <input 
+                  type="text" 
+                  placeholder='display name'
+                  name='displayName'
+                  value={values.displayName}
+                  onChange={changeHandler} 
+                />
+                <input 
+                  type="email" 
+                  placeholder='email'
+                  name='email'
+                  value={values.email}
+                  onChange={changeHandler} 
+                />
+                <input 
+                  type="password" 
+                  placeholder='******'
+                  name='password'
+                  value={values.password}
+                  onChange={changeHandler} 
+                />
                 {/* display none for input so that label is decorated */}
                 <label htmlFor="file">
                     <img src={Add} alt="" />
