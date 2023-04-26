bf18ebd5e19544a25a5985d3604011a65d1993ca
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-04-26 15:08:55 +0300

    custom greeting

--- simplechat/src/componentsHome/RightSide/Chat.js
+++ simplechat/src/componentsHome/RightSide/Chat.js
@@ -7,10 +7,11 @@ import { useChatContext } from '../../contexts/chatContext';
 const Chat = () => {
   const { chats } = useChatContext();
   const defaultGreeting = 'Select user to chat with...';
+  const customGreeting = `Chat buddy: ${chats.user?.displayName}`;
   return (
     <div className='chat'>
       <div className="chatInfo">
-        <span>{!!chats.user.displayName ? chats.user?.displayName : defaultGreeting}</span>
+        <span>{!!chats.user.displayName ? customGreeting : defaultGreeting}</span>
       </div>
       <Messages></Messages>
       <Input></Input>
